97427ef78895a59c1631f183f50ed607d9e66d69
carolin.labinski <carolin.labinski@yahoo.de>
2020-06-04 16:44:05 +0200

    add images

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,4 @@ Rails.application.routes.draw do
   resources :images
   devise_for :users
   root to: 'images#index'
-  # path: '',
-  # path_names: {
-  #   sign_in: 'login',
-  #   sign_out: 'logout',
-  #   registration: 'signup'
-  # },
-  # controllers: {
-  #   sessions: 'sessions',
-  #   registrations: 'registrations'
-  # }
-  
-  # get '/profile', to: 'profile#show'
-  # resources :users, only: [:show]
-
-
-  #resources :profile, defaults: {format: :json}
-  #get '/profile', to: 'profile#show' 
-  #put '/login', to: 'pages#home' 
-  #put '/profile', to: 'profile#show' 
- # get '/profile', to: 'profile#show' as:'profile'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
